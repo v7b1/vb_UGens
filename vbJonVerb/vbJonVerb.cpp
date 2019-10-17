6ae20db3687c0cf6d4c3c770333f9c1ec88bdcc1
v7b1 <vboehm@gmx.ch>
2019-10-17 20:26:45 +0200

    fix jonverb help

--- vbJonVerb/vbJonVerb.cpp
+++ vbJonVerb/vbJonVerb.cpp
@@ -13,21 +13,17 @@ static InterfaceTable *ft;
 // declare struct to hold unit generator state
 struct VBJonVerb : public Unit
 {
-	double mPhase; // phase of the oscillator, from -1 to 1.
-	float mFreqMul; // a constant for multiplying frequency
-    double        rate;
-    double        inputbandwidth;
+    //double        inputbandwidth;
     g_damper      *inputDamper;
     g_diffuser    **inputDiffusers;
     g_diffuser    **decayDiffusers1;
     g_diffuser    **decayDiffusers2;
     g_tapdelay    **tapdelays;
-    double        fbL, fbR, decay, erfl_gain, tail_gain;
+    double        fbL, fbR; //, decay, erfl_gain, tail_gain;
 };
 
 // declare unit generator functions
-static void VBJonVerb_next_a(VBJonVerb *unit, int inNumSamples);
-//static void VBJonVerb_next_k(VBJonVerb *unit, int inNumSamples);
+static void VBJonVerb_next(VBJonVerb *unit, int inNumSamples);
 static void VBJonVerb_Ctor(VBJonVerb *unit);
 static void VBJonVerb_Dtor(VBJonVerb *unit);
 
@@ -35,29 +31,11 @@ static void VBJonVerb_Dtor(VBJonVerb *unit);
 //////////////////////////////////////////////////////////////////
 
 
-// A Ctor usually does 3 things.
-// 1. set the calculation function.
-// 2. initialize the unit generator state variables.
-// 3. calculate one sample of output.
+
 static void VBJonVerb_Ctor(VBJonVerb *unit)
 {
-    /*
-	// 1. set the calculation function.
-	if (INRATE(0) == calc_FullRate) {
-		// if the frequency argument is audio rate
-		SETCALC(VBJonVerb_next_a);
-	} else {
-		// if the frequency argument is control rate (or a scalar).
-		SETCALC(VBJonVerb_next_k);
-	}*/
     
-    SETCALC(VBJonVerb_next_a);
-
-	// 2. initialize the unit generator state variables.
-	// initialize a constant for multiplying the frequency
-	unit->mFreqMul = 2.0 * SAMPLEDUR;
-	// get initial phase of oscillator
-	unit->mPhase = IN0(1);
+    SETCALC(VBJonVerb_next);
     
     //fixed delay taps
     int taps0[4] = {6598, 2949, 523, 5375};
@@ -75,11 +53,9 @@ static void VBJonVerb_Ctor(VBJonVerb *unit)
     int difftaps6[3] = {2667, 277, 1820};
     int difftaps7[3] = {3936, 2835, 496};
     
-    unit->erfl_gain = 0.5;
-    unit->tail_gain = 0.2;
-    unit->decay = 0.75;
-    unit->inputbandwidth = 0.5;
-    unit->inputDamper = damper_make(1.0-unit->inputbandwidth);
+
+    double inputbandwidth = IN0(3);
+    unit->inputDamper = damper_make(1.0 - inputbandwidth);
     unit->inputDiffusers = (g_diffuser **)calloc(DIFFORDER, sizeof(g_diffuser *));
     unit->inputDiffusers[0] = diffuser_make(1024, difftaps0, 0.75);
     unit->inputDiffusers[1] = diffuser_make(1024, difftaps1, 0.75);
@@ -106,7 +82,7 @@ static void VBJonVerb_Ctor(VBJonVerb *unit)
     unit->fbL = unit->fbR = 0.;
 
 	// 3. calculate one sample of output.
-	VBJonVerb_next_a(unit, 1);
+	VBJonVerb_next(unit, 1);
 }
 
 static void VBJonVerb_Dtor(VBJonVerb *unit)
@@ -126,11 +102,8 @@ static void VBJonVerb_Dtor(VBJonVerb *unit)
 
 //////////////////////////////////////////////////////////////////
 
-// The calculation function executes once per control period
-// which is typically 64 samples.
 
-// calculation function for an audio rate frequency argument
-void VBJonVerb_next_a(VBJonVerb *unit, int inNumSamples)
+void VBJonVerb_next(VBJonVerb *unit, int inNumSamples)
 {
 	// get the pointer to the output buffer
 	float *outL = OUT(0);
@@ -141,22 +114,18 @@ void VBJonVerb_next_a(VBJonVerb *unit, int inNumSamples)
     
     int vs = inNumSamples;
     double input, sumL, sumR, fbL, fbR, fbholdL;
-    double decay, erfl_gain, tail_gain, damping;
-    
+    double decay, erfl_gain, tail_gain, damping, input_bw;
     
     
     decay = IN0(1);
     damping = IN0(2);
-    
+    input_bw = IN0(3);
     erfl_gain = IN0(4);
     tail_gain = IN0(5);
     
     fbL = unit->fbL;
     fbR = unit->fbR;
-    //erfl_gain = unit->erfl_gain;
-    //tail_gain = unit->tail_gain;
-    //tail_gain *= tail_gain;    // make it a little softer...
-    //decay = unit->decay;
+
     sumL = sumR = 0.;
     g_damper *inputDamper = unit->inputDamper;
     g_diffuser **inputDiffs = unit->inputDiffusers;
@@ -164,7 +133,8 @@ void VBJonVerb_next_a(VBJonVerb *unit, int inNumSamples)
     g_diffuser **decayDiffs2 = unit->decayDiffusers2;
     g_tapdelay **tapDelays = unit->tapdelays;
     
-    
+    //inputDamper->damping = 1.0 - CLAMP(input_bw, 0., 0.997);
+    inputDamper->damping = 1.0 - input_bw;
     
     // first two tapdelays in reverb tail have a onepole filter at the feedback output
     tapDelays[0]->damping = damping;    //CLAMP(damping, 0., 1.);
@@ -229,51 +199,12 @@ void VBJonVerb_next_a(VBJonVerb *unit, int inNumSamples)
 
 //////////////////////////////////////////////////////////////////
 
-// calculation function for a control rate frequency argument
-/*
-void VBJonVerb_next_k(VBJonVerb *unit, int inNumSamples)
-{
-	// get the pointer to the output buffer
-	float *out = OUT(0);
-
-	// freq is control rate, so calculate it once.
-	float freq = IN0(0) * unit->mFreqMul;
-
-	// get phase from struct and store it in a local variable.
-	// The optimizer will cause it to be loaded it into a register.
-	double phase = unit->mPhase;
-
-	// since the frequency is not changing then we can simplify the loops
-	// by separating the cases of positive or negative frequencies.
-	// This will make them run faster because there is less code inside the loop.
-	if (freq >= 0.f) {
-		// positive frequencies
-		for (int i=0; i < inNumSamples; ++i)
-		{
-			out[i] = phase;
-			phase += freq;
-			if (phase >= 1.f) phase -= 2.f;
-		}
-	} else {
-		// negative frequencies
-		for (int i=0; i < inNumSamples; ++i)
-		{
-			out[i] = phase;
-			phase += freq;
-			if (phase <= -1.f) phase += 2.f;
-		}
-	}
-
-	// store the phase back to the struct
-	unit->mPhase = phase;
-}*/
-
 
 // the entry point is called by the host when the plug-in is loaded
 PluginLoad(VBJonVerb)
 {
-    // InterfaceTable *inTable implicitly given as argument to the load function
+
 	ft = inTable; // store pointer to InterfaceTable
 
-	DefineSimpleUnit(VBJonVerb);
+    DefineDtorUnit(VBJonVerb);
 }
