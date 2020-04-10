73a4c8d4b0a43529c42272fcb7268a63796d261b
v7b1 <vboehm@gmx.ch>
2020-04-10 22:39:18 +0200

    fix rings trigger and more

--- mi-dev/MiRings/MiRings.cpp
+++ mi-dev/MiRings/MiRings.cpp
@@ -5,7 +5,7 @@
 #include "rings/dsp/strummer.h"
 #include "rings/dsp/string_synth_part.h"
 #include "rings/dsp/dsp.h"
-#include "TriggerInput.h"
+//#include "TriggerInput.h"
 
 
 //float kSampleRate = 48000.0;
@@ -17,26 +17,23 @@ static InterfaceTable *ft;
 
 struct MiRings : public Unit {
     
-    rings::Part             part; //*part;
-    //rings::StringSynthPart  *string_synth;
+    rings::Part             part;
     rings::StringSynthPart  string_synth;
-    rings::Strummer         *strummer;
-    //rings::ReadInputs       read_inputs;
+    rings::Strummer         strummer;
     
-    double                  in_level;
+    float                   in_level;
     
     uint16_t                *reverb_buffer;
     
     rings::PerformanceState performance_state;
     rings::Patch            patch;
     
-    TriggerInp              trigger_input;
-    
     double                  sr;
     int                     sigvs;
     short                   strum_connected;
     short                   fm_patched;
-    
+    bool                    prev_trig;
+    int                     prev_poly;
     int                     pcount;
     
 };
@@ -50,9 +47,8 @@ void myObj_info(MiRings *unit);
 
 
 static void MiRings_Ctor(MiRings *unit) {
-    printf("MiRings constructor --!\n");
     
-    printf("sr: %f\n", SAMPLERATE);
+    Print("MiRings SR: %f\n", SAMPLERATE);
     //kSampleRate = SAMPLERATE;
     //a0 = (440.0f / 8.0f) / kSampleRate;
 
@@ -62,47 +58,30 @@ static void MiRings_Ctor(MiRings *unit) {
     // allocate memory
     unit->reverb_buffer = (uint16_t*)RTAlloc(unit->mWorld, 65536*sizeof(uint16_t));
 
-    // init with zeros?
-    //memset(unit->reverb_buffer, 0, 65536*sizeof(uint16_t));
+    // init with zeros
+    memset(unit->reverb_buffer, 0, 65536*sizeof(uint16_t));
 
-    unit->strummer = new rings::Strummer;
-    //unit->part = new rings::Part;
-    //unit->string_synth = new rings::StringSynthPart;
-    
-    //printf("sizeof strummer: %ld\n", sizeof(rings::Strummer));
-    //printf("sizeof strummer2 :%ld\n", sizeof(*MiRings::strummer));
-    printf("MiRings zero out --!\n");
-    memset(unit->strummer, 0, sizeof(rings::Strummer));
-    //memset(unit->part, 0, sizeof(rings::Part));
-    //memset(unit->string_synth, 0, sizeof(rings::StringSynthPart));
-    
-    printf("MiRings init --!\n");
-    unit->strummer->Init(0.01, rings::kSampleRate / kBlockSize);
+    unit->strummer.Init(0.01, rings::kSampleRate / kBlockSize);
     unit->part.Init(unit->reverb_buffer);
     unit->string_synth.Init(unit->reverb_buffer);
     
-    printf("MiRings set params --!\n");
-    
-    unit->part.set_polyphony(2);
+    unit->part.set_polyphony(1);
     unit->part.set_model(rings::RESONATOR_MODEL_MODAL);
-        //RESONATOR_MODEL_STRING_AND_REVERB
-        //RESONATOR_MODEL_FM_VOICE
-        //RESONATOR_MODEL_MODAL
-    unit->string_synth.set_polyphony(2);
-    unit->string_synth.set_fx(rings::FX_REVERB);
+
+    unit->string_synth.set_polyphony(1);
+    unit->string_synth.set_fx(rings::FX_FORMANT);
+    unit->prev_poly = 1;
     
     unit->performance_state.internal_exciter = false;
     unit->performance_state.internal_strum = true;
     unit->performance_state.internal_note = true;
+    unit->prev_trig = false;
     
     unit->pcount = 0;
     
-    printf("MiRings END constructor --!\n");
     SETCALC(MiRings_next);        // tells sc synth the name of the calculation function
     //MiRings_next(unit, 64);       // do we reallly need this?
-    
 
-    
 }
 
 
@@ -110,14 +89,10 @@ static void MiRings_Dtor(MiRings *unit) {
     
     SCWorld_Allocator alloc(ft, unit->mWorld);
 
-    delete unit->strummer;
-    //delete unit->part;
-    //delete unit->string_synth;
     if(unit->reverb_buffer) {
         RTFree(unit->mWorld, unit->reverb_buffer);
-        //free(unit->reverb_buffer);
     }
-    printf("done freeing memory!\n");
+    Print("done freeing memory!\n");
 }
 
 
@@ -126,16 +101,21 @@ static void MiRings_Dtor(MiRings *unit) {
 void MiRings_next( MiRings *unit, int inNumSamples)
 {
     
-    float *in = IN(0);
-    float *trig_in = IN(1);
+    float   *in = IN(0);
+    float   *trig_in = IN(1);
     
-    float voct_in = IN0(2);
-    float struct_in = IN0(3);
-    float bright_in = IN0(4);
-    float damp_in = IN0(5);
-    float pos_in = IN0(6);
-    bool  useTrigger = IN0(7);
-    bool bypass = IN0(8);
+    float   voct_in = IN0(2);
+    float   struct_in = IN0(3);
+    float   bright_in = IN0(4);
+    float   damp_in = IN0(5);
+    float   pos_in = IN0(6);
+    int     model = IN0(7);
+    int     polyphony = IN0(8);
+    bool    useTrigger = IN0(9);
+    bool    intern_exciter = IN0(10);
+    bool    intern_note = IN0(11);
+    bool    bypass = IN0(12);
+    
     
     float *out1 = OUT(0);
     float *out2 = OUT(1);
@@ -146,17 +126,28 @@ void MiRings_next( MiRings *unit, int inNumSamples)
     uint8_t    count = 0;
     rings::Patch *patch = &unit->patch;
     
+    unit->performance_state.internal_note = intern_note;
+    unit->performance_state.internal_exciter = intern_exciter;
+    
+    // set resonator model
+    CONSTRAIN(model, 0, 5);
+    unit->part.set_model(static_cast<rings::ResonatorModel>(model));
+    unit->string_synth.set_fx(static_cast<rings::FxType>(model));
+    
+    // set polyphony
+    if(polyphony != unit->prev_poly) {
+        CONSTRAIN(polyphony, 1, 4);
+        unit->part.set_polyphony(polyphony);
+        unit->string_synth.set_polyphony(polyphony);
+        unit->prev_poly = polyphony;
+    }
+    
+    // set pitch
     float pitch = abs(voct_in);
     CONSTRAIN(pitch, 0.f, 127.f);
-    unit->performance_state.note = pitch;
-    
-    float freq = 0.f;
-    CONSTRAIN(freq, -1.0f, 1.f);   // ??
-    float transpose = freq * 60.0f;
-    unit->performance_state.tonic = transpose + 12.0f;
-    
-    //unit->performance_state.tonic = 0.0f;
-    
+    unit->performance_state.tonic = pitch;
+    unit->performance_state.note = 0.0f;
+
 
     CONSTRAIN(struct_in, 0.0f, 0.9995f);
     patch->structure = struct_in;
@@ -175,41 +166,45 @@ void MiRings_next( MiRings *unit, int inNumSamples)
     CONSTRAIN(pos_in, 0.0f, 1.0f);
     patch->position = pos_in;
     
-    // we have to copy input vector (why?)
-    //for(int i=0; i<kBlockSize; i++)
-      //  input[i] = in[i];
-    std::copy(&in[0], &in[size], &input[0]);
+    if(INRATE(0) == calc_FullRate) {
+        // we have to copy input vector (why?)
+        std::copy(&in[0], &in[size], &input[0]);
+    }
+    else {
+        memset(input, 0, size*sizeof(float));
+    }
     
-    //unit->performance_state.strum = unit->trigger_input.rising_edge();
-    //unit->performance_state.strum = false;
-    
-    
-    //if(useTrigger && !unit->performance_state.internal_strum) {
+
     if(useTrigger) {
         unit->performance_state.internal_strum = false;
-        float trig = 0.f;
+        bool trig = false;
+        bool prev_trig = unit->prev_trig;
+        
         for(int i=0; i<inNumSamples; ++i) {
-            if(trig_in[i] > 0.001f) {
-                trig = 1.0f;
+            if(trig_in[i] > 0.f && !prev_trig) {
+                trig = true;
+                //printf("trigger\n");
                 break;
             }
         }
-        unit->trigger_input.Read(trig);
+        
+        unit->prev_trig = trig;
+        unit->performance_state.strum = trig;
     }
     else
         unit->performance_state.internal_strum = true;
     
-
+/*
     unit->pcount++;
     if(unit->pcount >= 2000) {
         unit->pcount = 0;
-        //myObj_info(unit);
+        myObj_info(unit);
     }
-    
+  */
 
     unit->part.set_bypass(bypass);
     
-    unit->strummer->Process(in, size, &unit->performance_state);
+    unit->strummer.Process(in, size, &unit->performance_state);
     //if(unit->performance_state.strum)
       //  printf("STRUM!\n");
     unit->part.Process(unit->performance_state, unit->patch, input, out1, out2, size);
@@ -268,7 +263,6 @@ void myObj_info(MiRings *unit)
     
     printf("polyphony: %d\n", pa->polyphony());
     printf("model: %d\n", pa->model());
-    //printf("easter_egg: %d\n", st->easter_egg);
     
     printf( "-----\n");
     
